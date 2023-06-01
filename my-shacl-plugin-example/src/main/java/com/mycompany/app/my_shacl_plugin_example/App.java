52275d38403b65fda1963e08fe93828e71b93f70
TanmayDevOps52 <113125685+TanmayDevOps52@users.noreply.github.com>
2023-06-01 12:12:10 +0530

    Add files via upload

---
+++ my-shacl-plugin-example/src/main/java/com/mycompany/app/my_shacl_plugin_example/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.my_shacl_plugin_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
